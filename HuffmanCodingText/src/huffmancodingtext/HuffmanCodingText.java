0ab95eb8f514365f985c5ac361d7ec5a97e06275
Alireza Sadraii <sadraiiali@gmail.com>
2017-05-09 14:18:43 +0430

    add simple gui

--- HuffmanCodingText/src/huffmancodingtext/HuffmanCodingText.java
+++ HuffmanCodingText/src/huffmancodingtext/HuffmanCodingText.java
@@ -4,21 +4,72 @@ package huffmancodingtext;
  *
  * @author piker
  */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import static java.util.Collections.sort;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javafx.util.Pair;
 
-public class HuffmanCodingText {
+public class HuffmanCodingText extends Application {
 
     /**
      * @param args the command line arguments
      */
     static ArrayList<Node> codingArray;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Input Text File");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Open input File");
+                try {
+                    runCoding(fileChooser.showOpenDialog(primaryStage),primaryStage);
+                } catch (IOException ex) {
+                    Logger.getLogger(HuffmanCodingText.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Huffman Coding | by Piker");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    static void runCoding(File inputFile,Stage primaryStage) throws IOException {
+
         Scanner input = new Scanner(System.in);
-        System.out.println("string ra vared konid \n");
-        String inputText = input.nextLine();
+        String inputText = readFile(inputFile);
         ArrayList<Node> chars = countChars(inputText);
         Collections.sort(chars);
         int numberOfChars = chars.size();
@@ -37,7 +88,20 @@ public class HuffmanCodingText {
         for (Node a : codingArray) {
             System.out.println(a);
         }
-        System.out.println(codeThisString(inputText));
+        String outString = codeThisString(inputText);
+//        System.out.println(outString);
+        byte[] bytesToWriteInFile = fromBinary(codeThisString(inputText));
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Coded File");
+        File file = fileChooser.showSaveDialog(primaryStage);
+        FileOutputStream fos = new FileOutputStream(file.getPath()+file.getName());
+        fos.write(bytesToWriteInFile);
+        fos.close();
+        Path path = Paths.get(file.getPath()+file.getName());
+        byte[] data = Files.readAllBytes(path);
+//        System.out.println(toBinary(data));
+//        System.out.println(decode(outString, head));
+
     }
 
     static ArrayList<Node> countChars(String input) {
@@ -71,7 +135,7 @@ public class HuffmanCodingText {
     }
 
     static String codeThisString(String inputString) {
-        String out="";
+        String out = "";
         for (int i = 0; i < inputString.length(); i++) {
             for (Node a : codingArray) {
                 if (a.value == inputString.charAt(i)) {
@@ -81,4 +145,69 @@ public class HuffmanCodingText {
         }
         return out;
     }
+
+    static String toBinary(byte[] bytes) {
+        StringBuilder sb = new StringBuilder(bytes.length * Byte.SIZE);
+        for (int i = 0; i < Byte.SIZE * bytes.length; i++) {
+            sb.append((bytes[i / Byte.SIZE] << i % Byte.SIZE & 0x80) == 0 ? '0' : '1');
+        }
+        return sb.toString();
+    }
+
+    static byte[] fromBinary(String s) {
+        int sLen = s.length();
+        byte[] toReturn = new byte[(sLen + Byte.SIZE - 1) / Byte.SIZE];
+        char c;
+        for (int i = 0; i < sLen; i++) {
+            if ((c = s.charAt(i)) == '1') {
+                toReturn[i / Byte.SIZE] = (byte) (toReturn[i / Byte.SIZE] | (0x80 >>> (i % Byte.SIZE)));
+            } else if (c != '0') {
+                throw new IllegalArgumentException();
+            }
+        }
+        return toReturn;
+    }
+
+    static String decode(String input, Node head) {
+        String out = "";
+        for (int i = 0; i < input.length();) {
+            Node temp = head;
+            try {
+                while (temp.left != null || temp.right != null) {
+                    if (input.charAt(i) == '1') {
+                        temp = temp.right;
+                        i++;
+                    } else {
+                        temp = temp.left;
+                        i++;
+                    }
+                }
+            } catch (Exception a) {
+                System.out.println(" error ");
+            }
+            out += temp.value;
+        }
+        return out;
+    }
+
+    static String readFile(File inputFile) {
+        String out = "";
+        try {
+            File file = inputFile;
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuffer.append(line);
+                stringBuffer.append("\n");
+            }
+            fileReader.close();
+            out = stringBuffer.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out;
+    }
+
 }
